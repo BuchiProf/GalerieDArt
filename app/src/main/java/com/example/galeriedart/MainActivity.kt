2d96c41f227db51386aee048e84573288176aa80
BuchiProf <profbuchi@gmail.comm>
2024-11-30 17:17:10 +0100

    finalisation version statique

--- app/src/main/java/com/example/galeriedart/MainActivity.kt
+++ app/src/main/java/com/example/galeriedart/MainActivity.kt
@@ -38,54 +38,63 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-
-@Composable
-fun PageWithImageTextAndButtons(modifier: Modifier=Modifier){
-    Column(
-        modifier = modifier
-            .wrapContentSize()
-            .padding(26.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painterResource(
-                R.drawable._40px_the_gallery_of_cornelis_van_der_geest),
+    @Composable
+    fun PageWithImageTextAndButtons(modifier: Modifier = Modifier) {
+        Column(
+            modifier = modifier
+                .wrapContentSize()
+                .padding(26.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(
+                    R.drawable._40px_the_gallery_of_cornelis_van_der_geest
+                ),
                 contentDescription = "tableau"
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Column(modifier = Modifier,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = stringResource(R.string.titre),
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center)
-            Row(modifier = Modifier.fillMaxWidth()){
-                Spacer(modifier = Modifier.weight(1f))
-                Text(text = "auteur",
-                    Modifier.padding(horizontal = 5.dp))
-                Text(text = "date",
-                    Modifier.padding(horizontal = 5.dp))
-                Spacer(modifier = Modifier.weight(1f))
+            Column(
+                modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.titre1),
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
+                )
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = "auteur",
+                        Modifier.padding(horizontal = 5.dp)
+                    )
+                    Text(
+                        text = "date",
+                        Modifier.padding(horizontal = 5.dp)
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(modifier = Modifier) {
+                Button(
+                    onClick = {/*todo*/ }
+                ) { Text(text = "gauche") }
+                Spacer(Modifier.weight(1f))
+                Button(onClick = {/*todo*/ }) { Text(text = "droite") }
             }
         }
-        Spacer(modifier=Modifier.height(16.dp))
-
-        Row(modifier = Modifier) {
-            Button(
-                onClick = {/*todo*/}
-            ) { Text(text = "gauche") }
-            Spacer(Modifier.weight(1f))
-            Button(onClick = {/*todo*/}) { Text(text = "droite") }
-        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun ArtGalleryApp() {
-    PageWithImageTextAndButtons(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun ArtGalleryApp() {
+        PageWithImageTextAndButtons(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
+    }
 }
